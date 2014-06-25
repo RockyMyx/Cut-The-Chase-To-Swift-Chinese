312acfa13b5742951ca2b448234b6695408bba12
RockyMyx <myx8178633@163.com>
2014-06-25 17:32:51 +0800

    update to extension

--- content.swift
+++ content.swift
@@ -1200,7 +1200,112 @@ extension SomeType: SomeProtocol, AnotherProctocol {
     // 协议实现写到这里
 }
 
+扩展可以添加新的计算属性，但是不可以添加存储属性，也不可以向已有属性添加属性观测器(property observers)。
 
+扩展能向类中添加新的便利构造器，但是它们不能向类中添加新的指定构造器或析构函数。指定构造器和析构函数必须总是由原始的类实现来提供。
+
+如果你使用扩展向一个值类型添加一个构造器，在该值类型已经向所有的存储属性提供默认值，而且没有定义任何定制构造器（custom initializers）时，你可以在值类型的扩展构造器中调用默认构造器(default initializers)和逐一成员构造器(memberwise initializers)。  
+正如在值类型的构造器委托中描述的，如果你已经把构造器写成值类型原始实现的一部分，上述规则不再适用。
+
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+let centerRect = Rect(center: Point(x: 4.0, y: 4.0), size: Size(width: 3.0, height: 3.0))
+// centerRect的原点是 (2.5, 2.5)，大小是 (3.0, 3.0)
+
+扩展可以向已有类型添加新的实例方法和类型方法。下面的例子向Int类型添加一个名为repetitions的新实例方法：
+
+extension Int {
+    func repetitions(task: () -> ()) {
+        for i in 0..self {
+            task()
+        }
+    }
+}
+
+3.repetitions({println("Hello!")})
+//或者3.repetitions{println("Hello!")}
+// Hello!
+// Hello!
+// Hello!
+
+结构体和枚举类型中修改self或其属性的方法必须将该实例方法标注为mutating 
+extension Int {
+    mutating func square() {
+        self = self * self
+    }
+}
+var someInt = 3
+someInt.square()
+// someInt 现在值是 9
+
+extension Int {
+    subscript(digitIndex: Int) -> Int {
+        var decimalBase = 1
+            for _ in 1...digitIndex {
+                decimalBase *= 10
+            }
+        return (self / decimalBase) % 10
+    }
+}
+746381295[0]
+// returns 5
+746381295[1]
+// returns 9
+
+//Int值没有足够的位数，即下标越界，那么上述实现的下标会返回0，因为它会在数字左边自动补0：
+746381295[9]
+//returns 0
+
+
+extension Character {
+    enum Kind {
+        case Vowel, Consonant, Other
+    }
+    var kind: Kind {
+        switch String(self).lowercaseString {
+        case "a", "e", "i", "o", "u":
+            return .Vowel
+        case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+             "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
+            return .Consonant
+        default:
+            return .Other
+        }
+    }
+}
+func printLetterKinds(word: String) {
+    println("'\\(word)' is made up of the following kinds of letters:")
+    for character in word {
+        switch character.kind {
+        case .Vowel:
+            print("vowel ")
+        case .Consonant:
+            print("consonant ")
+        case .Other:
+            print("other ")
+        }
+    }
+    print("\n")
+}
+printLetterKinds("Hello")
+// 'Hello' is made up of the following kinds of letters:
+// consonant vowel consonant consonant vowel
 
 
 
